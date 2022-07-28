d79d9000d869728cfa52caecc32f4b883efbfe1a
chrisking718 <chrisking@pursuit.org>
2022-07-28 15:02:59 -0400

    04 passed

--- src/04-find.js
+++ src/04-find.js
@@ -1,3 +1,66 @@
+const songs = [
+  {
+    title: "Berlin Tsukin",
+    album: "Bi-To Te-Pu",
+    artist: "Taiyo Ky",
+    runtimeInSeconds: 193,
+  },
+  {
+    title: "Up",
+    album: "Bluewerks Vol. 1: Up Down Left Right",
+    artist: "Sebastian Kamae",
+    runtimeInSeconds: 120,
+  },
+  {
+    title: "Daylight",
+    album: "Bluewerks Vol. 1: Up Down Left Right",
+    artist: "hiyasu",
+    runtimeInSeconds: 154,
+  },
+  {
+    title: "Dormancy",
+    album: "Dormancy",
+    artist: "Arcade Portal",
+    runtimeInSeconds: 126,
+  },
+  {
+    title: "Pineapple Jam",
+    album: "Bluewerks Vol. 1: Up Down Left Right",
+    artist: "Saib",
+    runtimeInSeconds: 186,
+  },
+  {
+    title: "Samui Sunrise",
+    album: "Samui Sunrise",
+    artist: "Saib",
+    runtimeInSeconds: 204,
+  },
+  {
+    title: "Regular Guy",
+    album: "Regular Guy",
+    artist: "Peanut",
+    runtimeInSeconds: 215,
+  },
+  {
+    title: "In the Middle of Nowhere",
+    album: "It's OK",
+    artist: "Soulaced",
+    runtimeInSeconds: 123,
+  },
+  {
+    title: "Pink Elephants",
+    album: "Waterfalls",
+    artist: "Timestreet",
+    runtimeInSeconds: 171,
+  },
+  {
+    title: "Kyona",
+    album: "Kyona",
+    artist: "Two Sleepy",
+    runtimeInSeconds: 188,
+  },
+];
+
 /*
   Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
 
@@ -13,21 +76,30 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  return found = songs.find(({title})=> title === "Pink Elephants")
+  
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  return found = songs.find(({runtimeInSeconds})=> runtimeInSeconds < 180)
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  return (found = songs.find(({title,album})=> title === album))
+}
+
+findFirstTitleTrack(songs)
 
 module.exports = {
   findPinkElephantsByTimestreet,
